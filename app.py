b7ee7b81b3201fa4e8bbc6ecdc2d789f24c7850f
rafael747cardoso <rafael747cardoso@gmail.com>
2021-11-24 19:07:51 -0300

    filters and table going

--- app.py
+++ app.py
@@ -15,12 +15,8 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 import operator
-import dash
-from dash import dash_table
-from dash import dcc
-from dash import html
+from dash import Dash, dcc, html, dash_table, Input, Output, State
 import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output
 import base64
 from funcs.content_page_home import content_page_home
 from funcs.content_page_table import content_page_table
@@ -32,18 +28,22 @@ pd.set_option("display.max_columns", 300)
 pd.set_option("display.max_rows", 300)
 
 
-
 #----------------------------------------------------------------------------------------------------------------------
 #################################################### Data #############################################################
 
 # Read the data:
 df_videos = dt.fread(path_data + "videos_data.csv", sep = ";").to_pandas()
 
+df_videos = df_videos[(df_videos["channel_title"] == "Mustard") | (df_videos["channel_title"] == "Steve Cutts")]
+
+
 # Selects options:
-vars_cat_opts = [
+vars_poss_filter_cat = [
+    {"label": "Variable", "value": "Variable"},
     {"label": "Channel", "value": "channel_title"}
 ]
-vars_num_opts = [
+vars_poss_filter_num = [
+    {"label": "Variable", "value": "Variable"},
     {"label": "Views", "value": "views"},
     {"label": "Likes", "value": "likes"},
     {"label": "Dislikes", "value": "dislikes"},
@@ -66,7 +66,7 @@ vars_num_opts = [
     {"label": "Mean comments/likes per day", "value": "mean_comments_likes_ratio_day"},
     {"label": "Mean comments/dislikes per day", "value": "mean_comments_dislikes_ratio_day"}
 ]
-operations_opts = [
+filter_operations_poss = [
     {"label": "Operator", "value": "Operator"},
     {"label": ">=", "value": ">="},
     {"label": ">", "value": ">"},
@@ -74,14 +74,15 @@ operations_opts = [
     {"label": "<", "value": "<"},
     {"label": "<=", "value": "<="}
 ]
-operations_funcs = {
+ops = {
     ">=": operator.ge,
     ">": operator.gt,
     "==": operator.eq,
     "<": operator.lt,
     "<=": operator.le
 }
-
+cols_names = [i["value"] for i in vars_poss_filter_cat[1:]] + [i["value"] for i in vars_poss_filter_num[1:]]
+nice_names = [i["label"] for i in vars_poss_filter_cat[1:]] + [i["label"] for i in vars_poss_filter_num[1:]]
 
 
 
@@ -89,30 +90,151 @@ operations_funcs = {
 #----------------------------------------------------------------------------------------------------------------------
 ################################################# Initialize ##########################################################
 
-app = dash.Dash(
+app = Dash(
     name = __name__,
-    external_stylesheets = ["assets/bootstrap.css"]
+    external_stylesheets = ["assets/bootstrap.css"],
+    suppress_callback_exceptions = True
 )
 server = app.server
 
 #----------------------------------------------------------------------------------------------------------------------
 #################################################### Backend ##########################################################
 
-###### Exploratory Data Analysis
+#################################################### Table
 
-### Table
+# Update the options for the categorical filter select:
+# @app.callback(
+#     Output(component_id = "table_filter_cat_var_value", component_property = "options"),
+#     [
+#         Input(component_id = "table_filter_cat_var_name", component_property = "value")
+#     ]
+# )
+# def update_select_filter_cat(table_filter_cat_var_name):
+#     opt0 = [{"label": "", "value": ""}]
+#     if table_filter_cat_var_name in df_videos.columns:
+#         opts = [{"label": l, "value": l} for l in df_videos[table_filter_cat_var_name].unique()]
+#     else:
+#         opts = [{"label": "", "value": ""}]
+#     return(opt0 + opts)
 
-# @app.callback()
+# Apply the filters on the table:
+# @app.callback(
+#     Output(component_id = "dataset-table", component_property = "children"),
+#     [
+#         Input(component_id = "table_filter_cat_var_name", component_property = "value"),
+#         Input(component_id = "table_filter_cat_var_value", component_property = "value"),
+#         Input(component_id = "table_filter_num_var_name", component_property = "value"),
+#         Input(component_id = "table_filter_num_operation", component_property = "value"),
+#         Input(component_id = "table_filter_num_var_value", component_property = "value")
+#      ]
+# )
+# def update_table(table_filter_cat_var_name,
+#                  table_filter_cat_var_value,
+#                  table_filter_num_var_name,
+#                  table_filter_num_operation,
+#                  table_filter_num_var_value):
+#     # Filters:
+#     df = df_videos.copy()[cols_names]
+#     if table_filter_num_var_name != "Variable" and \
+#        table_filter_num_operation != "Operator":
+#         op_func = ops[table_filter_num_operation]
+#         df = df[op_func(df[table_filter_num_var_name], table_filter_num_var_value)]
+#     if table_filter_cat_var_name != "Variable" and \
+#             table_filter_cat_var_value != "Levels" and \
+#             table_filter_cat_var_value != "":
+#         df = df[df[table_filter_cat_var_name] == table_filter_cat_var_value]
+#     df.columns = nice_names
+# 
+#     # Table:
+#     table = dash_table.DataTable(
+#         id = "dataset-table",
+#         columns = [{"name": c, "id": c} for c in df.columns],
+#         data = df.to_dict("records"),
+#         page_size = 18,
+#         style_as_list_view = True,
+#         style_header = {
+#             "backgroundColor": "rgb(30, 30, 30)"
+#         },
+#         style_cell = {
+#             "backgroundColor": "rgb(50, 50, 50)",
+#             "color": "white",
+#             "textAlign": "center"
+#         },
+#         style_cell_conditional = [
+#             {"if": {"column_id": ["Manufacturer Name",
+#                                   "Model Name"]},
+#              "textAlign": "left"}
+#         ],
+#         style_table = {
+#             "overflowX": "auto"
+#         }
+#     )
+#     return (table)
+
+# @app.callback(
+#     Output(component_id = "test_output", component_property = "children"),
+#     [
+#         Input(component_id = "test_input", component_property = "value")
+#     ]
+# )
+# def update_output(x):
+#     y = x**2
+#     return(y)
+
+@app.callback(
+    Output(component_id = "test_output", component_property = "children"),
+    [
+        Input(component_id = "bnt-apply-filters", component_property = "n_clicks"),
+    ],
+    State(component_id = "test_input", component_property = "value")
+)
+def update_output(n_clicks, x):
+    y = x**2
+    return(y)
+
+    
+
+# @app.callback(
+#     Output(component_id = "dataset-table", component_property = "children"),
+#     [
+#         Input(component_id = "bnt-apply-filters", component_property = "n_clicks"),
+#     ],
+#     State(component_id = "table_filter_num_var_value", component_property = "value")
+# )
+# def update_table(n_clicks, table_filter_num_var_value):
+#     # Apply the filters:
+#     df = df_videos.copy()[cols_names]
+#     df = df[df["views"] > table_filter_num_var_value]
+#     
+#     # Table:
+#     table = dash_table.DataTable(
+#         id = "dataset-table",
+#         columns = [{"name": c, "id": c} for c in df.columns],
+#         data = df.to_dict("records"),
+#         page_size = 18,
+#         style_as_list_view = True,
+#         style_header = {
+#             "backgroundColor": "rgb(30, 30, 30)"
+#         },
+#         style_cell = {
+#             "backgroundColor": "rgb(50, 50, 50)",
+#             "color": "white",
+#             "textAlign": "center"
+#         },
+#         style_cell_conditional = [
+#             {"if": {"column_id": ["Manufacturer Name",
+#                                   "Model Name"]},
+#              "textAlign": "left"}
+#         ],
+#         style_table = {
+#             "overflowX": "auto"
+#         }
+#     )
+#     return (table)
 
 
 
-
-
-
-
-
-
-### Plots
+#################################################### Plots
 
 # Barplot of the number of videos by channel:
 
@@ -142,7 +264,6 @@ server = app.server
 # Images for logo and tabs:
 logo_filename = "dash_logo.png"
 logo_image = base64.b64encode(open(logo_filename, "rb").read())
-
 home_tab_filename = "home_tab.png"
 home_tab_image = base64.b64encode(open(home_tab_filename, "rb").read())
 table_tab_filename = "table_tab.png"
@@ -269,7 +390,11 @@ def render_page_content(pathname):
     if pathname == "/":
         return content_page_home()
     elif pathname == "/page_table":
-        return content_page_table()
+        return content_page_table(
+        #     vars_poss_filter_cat = vars_poss_filter_cat,
+        #     vars_poss_filter_num = vars_poss_filter_num,
+        #     filter_operations_poss = filter_operations_poss
+        )
     elif pathname == "/page_plots":
         return content_page_plots()
 
--- funcs/content_page_table.py
+++ funcs/content_page_table.py
@@ -14,22 +14,193 @@ from dash import dcc
 from dash import html
 import dash_bootstrap_components as dbc
 
-def content_page_table():
-    
-    pg = [
-        html.Div(
-            
-            "Here goes the table page",
-            
-            
-            
-            
-            
-            
-            
-            className = "welcome-page"
-        )
-    ]
+def content_page_table(
+        # vars_poss_filter_cat,
+        # vars_poss_filter_num,
+        # filter_operations_poss
+):
+    # """
+    # Makes the UI of the Table tab.
+    # :param vars_poss_filter_cat:
+    # :param vars_poss_filter_num:
+    # :param filter_operations_poss:
+    # :return: 
+    # """
 
-    return (pg)
+    # pg = [
+    #     html.Div(
+    #         [
+    #             html.Br(),
+    #             dbc.Row(
+    #                 [
+    #                     dbc.Col(
+    #                         [
+    #                             dbc.Card(
+    #                                 [
+    #                                     # Filters:
+    #                                     dbc.Row(
+    #                                         [
+    #                                             # Categoric:
+    #                                             dbc.Col(
+    #                                                 [
+    #                                                     dbc.Card(
+    #                                                         [
+    #                                                             dbc.Row(
+    #                                                                 [
+    #                                                                     dbc.Col(
+    #                                                                         [
+    #                                                                             html.Div(
+    #                                                                                 "Categoric filter",
+    #                                                                                 className = "filter-title"
+    #                                                                             )
+    #                                                                         ],
+    #                                                                         width = 12
+    #                                                                     ),
+    #                                                                     dbc.Col(
+    #                                                                         [
+    #                                                                             dbc.Select(
+    #                                                                                 id = "table_filter_cat_var_name",
+    #                                                                                 options = vars_poss_filter_cat,
+    #                                                                                 value = vars_poss_filter_cat[0]["value"]
+    #                                                                             )
+    #                                                                         ],
+    #                                                                         width = 6
+    #                                                                     ),
+    #                                                                     dbc.Col(
+    #                                                                         [
+    #                                                                             dbc.Select(
+    #                                                                                 id = "table_filter_cat_var_value",
+    #                                                                                 options = [
+    #                                                                                     {
+    #                                                                                         "label": "",
+    #                                                                                         "value": ""
+    #                                                                                     }
+    #                                                                                 ],
+    #                                                                                 value = ""
+    #                                                                             )
+    #                                                                         ],
+    #                                                                         width = 6
+    #                                                                     )
+    #                                                                 ]
+    #                                                             )
+    #                                                         ],
+    #                                                         className = "card-container"
+    #                                                     )
+    #                                                 ],
+    #                                                 width = 6
+    #                                             ),
+    #                                             # Numeric:
+    #                                             dbc.Col(
+    #                                                 [
+    #                                                     dbc.Card(
+    #                                                         [
+    #                                                             dbc.Row(
+    #                                                                 [
+    #                                                                     dbc.Col(
+    #                                                                         [
+    #                                                                             html.Div(
+    #                                                                                 "Numeric filter",
+    #                                                                                 className = "filter-title"
+    #                                                                             )
+    #                                                                         ],
+    #                                                                         width = 12
+    #                                                                     ),
+    #                                                                     dbc.Col(
+    #                                                                         [
+    #                                                                             dbc.Select(
+    #                                                                                 id = "table_filter_num_var_name",
+    #                                                                                 options = vars_poss_filter_num,
+    #                                                                                 value = vars_poss_filter_num[0]["value"]
+    #                                                                             )
+    #                                                                         ],
+    #                                                                         width = 6
+    #                                                                     ),
+    #                                                                     dbc.Col(
+    #                                                                         [
+    #                                                                             dbc.Select(
+    #                                                                                 id = "table_filter_num_operation",
+    #                                                                                 options = filter_operations_poss,
+    #                                                                                 value = filter_operations_poss[0]["value"]
+    #                                                                             )
+    #                                                                         ],
+    #                                                                         width = 3
+    #                                                                     ),
+    #                                                                     dbc.Col(
+    #                                                                         [
+    #                                                                             dbc.Input(
+    #                                                                                 id = "table_filter_num_var_value",
+    #                                                                                 type = "number",
+    #                                                                                 value = 0
+    #                                                                             )
+    #                                                                         ],
+    #                                                                         width = 3
+    #                                                                     )
+    #                                                                 ]
+    #                                                             )
+    #                                                         ],
+    #                                                         className = "card-container"
+    #                                                     )
+    #                                                 ],
+    #                                                 width = 6
+    #                                             )
+    #                                         ]
+    #                                     ),
+    #                                     # Button to run the filters:
+    #                                     dbc.Button(
+    #                                         "Apply the filters",
+    #                                         id = "bnt-apply-filters",
+    #                                         className = "me-2",
+    #                                         n_clicks = 0
+    #                                     ),
+    #                                     # Table:
+    #                                     dbc.Row(
+    #                                         [
+    #                                             dbc.Col(
+    #                                                 [
+    #                                                     html.Div(
+    #                                                         id = "dataset-table",
+    #                                                         children = "",
+    #                                                         className = "table-data"
+    #                                                     )
+    #                                                 ]
+    #                                             )
+    #                                         ]
+    #                                     )
+    #                                 ],
+    #                                 className = "card-container",
+    #                                 inverse = True
+    #                             )
+    #                         ],
+    #                         width = 12
+    #                     )
+    #                 ]
+    #             )
+    #         ],
+    #         className = "table-page"
+    #     )
+    # ]
+    
+    pg2 = html.Div(
+        [
+            dbc.Input(
+                id = "test_input",
+                type = "number",
+                value = 10
+            ),
+            
+            dbc.Button(
+                "Apply the filters",
+                id = "bnt-apply-filters",
+                className = "me-2",
+                n_clicks = 0
+            ),
+            
+            html.Div(
+                id = "test_output",
+                children = ""
+            )
+        ]
+    )
+    
+    return (pg2)
 
